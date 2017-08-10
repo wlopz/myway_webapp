ae4948d18dda70d3ae800d695941951547fb12e6
Wilfredo Lopez <jrlopez85@gmail.com>
2017-08-10 13:04:40 -0700

    unicorn.rb modified

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,16 +1,10 @@
-require 'fileutils'
-preload_app true
+
 
 working_directory "/vagrant/"
 pid "/vagrant/tmp/pids/unicorn.pid"
 stderr_path "/vagrant/log/unicorn.log"
 stdout_path "/vagrant/log/unicorn.log"
 
-listen "/tmp/unicorn.mywayNginx.sock", backlog: 1024
-
-before_fork do |server,worker|
-  FileUtils.touch('/tmp/app-initialized')
-end
-
+listen "/tmp/unicorn.mywayNginx.sock"
 worker_processes 2
 timeout 30
