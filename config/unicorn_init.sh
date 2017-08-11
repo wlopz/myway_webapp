04dc0073b8b862f1e5a73ce504160c7e8886ec38
Wilfredo Lopez <jrlopez85@gmail.com>
2017-08-10 20:23:57 -0700

    unicorn shell modified

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -20,7 +20,7 @@ APP_ROOT="$APP_NAME/"
 ENV="production"
 
 # environment settings
-CMD="cd $APP_ROOT && bundle exec unicorn -c config/unicorn.rb -E $ENV -D"
+CMD="bundle exec unicorn -c config/unicorn.rb -E $ENV -D"
 PID="$APP_ROOT/shared/pids/unicorn.pid"
 OLD_PID="$PID.oldbin"
 
